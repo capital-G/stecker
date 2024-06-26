db5ffec9cb87d55151af98bbc896480cee348091
Dennis Scheiba <dennis.scheiba@gmx.de>
2024-06-26 21:37:37 +0200

    remove shared models

--- src/shared/src/lib.rs
+++ src/shared/src/lib.rs
@@ -1,3 +1,2 @@
 pub mod connections;
-pub mod models;
 pub mod utils;
--- src/shared/src/models.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::Arc;
-use webrtc::{data_channel::RTCDataChannel, peer_connection::RTCPeerConnection};
-
-#[derive(Clone)]
-pub struct Connection {
-    pub peer_connection: Arc<RTCPeerConnection>,
-    pub data_channel: Option<Arc<RTCDataChannel>>,
-}
