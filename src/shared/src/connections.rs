ee09fafbeaeece4ff6b7e51beba220321907ccc5
Dennis Scheiba <dennis.scheiba@gmx.de>
2024-06-26 21:36:15 +0200

    remove legacy connection

--- src/shared/src/connections.rs
+++ src/shared/src/connections.rs
@@ -1,8 +1,6 @@
-use crate::models::Connection;
 use crate::utils::{decode_b64, encode_offer};
 
 use tokio::sync::broadcast;
-use tokio::sync::mpsc::Sender;
 use tokio::sync::Mutex;
 use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
 use webrtc::peer_connection::RTCPeerConnection;
@@ -16,111 +14,6 @@ use webrtc::data_channel::RTCDataChannel;
 use webrtc::ice_transport::ice_server::RTCIceServer;
 use webrtc::interceptor::registry::Registry;
 use webrtc::peer_connection::configuration::RTCConfiguration;
-
-pub struct ConnectionWithOffer {
-    pub connection: Arc<Mutex<Connection>>,
-    pub offer: String,
-}
-
-// FIXME: is this obsolete now?
-impl Connection {
-    pub async fn respond_to_offer(
-        offer: String,
-        tx: Arc<Sender<String>>,
-    ) -> anyhow::Result<ConnectionWithOffer> {
-        let desc_data = decode_b64(&offer)?;
-        let offer = serde_json::from_str::<RTCSessionDescription>(&desc_data)?;
-
-        let mut m = MediaEngine::default();
-        m.register_default_codecs()?;
-
-        let mut registry = Registry::new();
-        registry = register_default_interceptors(registry, &mut m)?;
-
-        let api = APIBuilder::new()
-            .with_media_engine(m)
-            .with_interceptor_registry(registry)
-            .build();
-
-        let config = RTCConfiguration {
-            ice_servers: vec![RTCIceServer {
-                urls: vec!["stun:stun.l.google.com:19302".to_owned()],
-                ..Default::default()
-            }],
-            ..Default::default()
-        };
-
-        let peer_connection = Arc::new(api.new_peer_connection(config).await?);
-
-        let connection = Arc::new(Mutex::new(Connection {
-            peer_connection: peer_connection.clone(),
-            data_channel: None,
-        }));
-
-        let connection_clone = connection.clone();
-
-        peer_connection.on_data_channel(Box::new(move |d: Arc<RTCDataChannel>| {
-            let tx2 = tx.clone();
-
-            d.on_close(Box::new(|| {
-                println!("Data channel closed");
-                Box::pin(async {})
-            }));
-
-            d.on_open(Box::new(move || {
-                println!("Opened channel");
-                Box::pin(async move {})
-            }));
-
-            d.on_message(Box::new(move |message: DataChannelMessage| {
-                let msg_str = String::from_utf8(message.data.to_vec()).unwrap();
-                let msg2 = msg_str.clone();
-
-                let tx3 = tx2.clone();
-
-                Box::pin(async move {
-                    match tx3.send(msg2).await {
-                        Ok(_) => (),
-                        Err(x) => println!("Could not forward message: {x}"),
-                    }
-                })
-            }));
-
-            let c2 = connection.clone();
-
-            Box::pin(async move {
-                c2.lock().await.data_channel = Some(d.clone());
-            })
-        }));
-
-        peer_connection.set_remote_description(offer).await?;
-        let answer = peer_connection.create_answer(None).await?;
-
-        // Create channel that is blocked until ICE Gathering is complete
-        let mut gather_complete = peer_connection.gathering_complete_promise().await;
-
-        // Sets the LocalDescription, and starts our UDP listeners
-        peer_connection.set_local_description(answer).await?;
-
-        // Block until ICE Gathering is complete, disabling trickle ICE
-        // we do this because we only can exchange one signaling message
-        // in a production application you should exchange ICE Candidates via OnICECandidate
-        let _ = gather_complete.recv().await;
-
-        // Output the answer in base64 so we can paste it in browser
-        let offer = if let Some(local_desc) = peer_connection.local_description().await {
-            let b64 = encode_offer(local_desc)?;
-            Ok(b64)
-        } else {
-            Err(anyhow::anyhow!("Error while creating RTC offer"))
-        };
-
-        Ok(ConnectionWithOffer {
-            connection: connection_clone,
-            offer: offer?,
-        })
-    }
-}
 
 pub struct SteckerWebRTCConnection {
     // FIXME: potentially both fields are obsolete
@@ -203,49 +96,50 @@ impl SteckerWebRTCConnection {
         let (out_tx, _out_rx) = broadcast::channel::<String>(1);
         let out_tx_clone = out_tx.clone();
         let in_tx_clone = in_tx.clone();
-    
-        self.peer_connection.on_data_channel(Box::new(move |d: Arc<RTCDataChannel>| {
-            let tx2 = in_tx.clone();
-            let out_tx2 = out_tx.clone();
-    
-            d.on_close(Box::new(|| {
-                println!("Data channel closed");
-                Box::pin(async {})
-            }));
-    
-            d.on_open(Box::new(move || {
-                println!("Opened channel");
-                Box::pin(async move {})
-            }));
-    
-            d.on_message(Box::new(move |message: DataChannelMessage| {
-                let msg_str = String::from_utf8(message.data.to_vec()).unwrap();
-                let tx3 = tx2.clone();
-    
+
+        self.peer_connection
+            .on_data_channel(Box::new(move |d: Arc<RTCDataChannel>| {
+                let tx2 = in_tx.clone();
+                let out_tx2 = out_tx.clone();
+
+                d.on_close(Box::new(|| {
+                    println!("Data channel closed");
+                    Box::pin(async {})
+                }));
+
+                d.on_open(Box::new(move || {
+                    println!("Opened channel");
+                    Box::pin(async move {})
+                }));
+
+                d.on_message(Box::new(move |message: DataChannelMessage| {
+                    let msg_str = String::from_utf8(message.data.to_vec()).unwrap();
+                    let tx3 = tx2.clone();
+
+                    Box::pin(async move {
+                        let _ = tx3.send(msg_str);
+                    })
+                }));
+
                 Box::pin(async move {
-                    let _ = tx3.send(msg_str);
-                })
-            }));
-    
-            Box::pin(async move {
-                tokio::spawn(async move {
-                    let mut out_rx2 = out_tx2.subscribe();
-    
-                    while let Ok(msg) = out_rx2.recv().await {
-                        println!("Sending out something: {}", msg.clone());
-                        match d.send_text(msg).await {
-                            Ok(_) => {}
-                            Err(err) => {
-                                println!(
+                    tokio::spawn(async move {
+                        let mut out_rx2 = out_tx2.subscribe();
+
+                        while let Ok(msg) = out_rx2.recv().await {
+                            println!("Sending out something: {}", msg.clone());
+                            match d.send_text(msg).await {
+                                Ok(_) => {}
+                                Err(err) => {
+                                    println!(
                                     "ERROR: forwarding message from channel to data_channel {err}"
                                 );
+                                }
                             }
                         }
-                    }
-                });
-            })
-        }));
-    
+                    });
+                })
+            }));
+
         (out_tx_clone, in_tx_clone)
     }
 }
