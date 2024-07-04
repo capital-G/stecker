535d8478df8b9041d602fb983ef7bafbf269d96f
Dennis Scheiba <dennis.scheiba@gmx.de>
2024-07-04 21:28:04 +0200

    introduce async mutex for sc plugin

--- src/sc_plugin/src/lib.rs
+++ src/sc_plugin/src/lib.rs
@@ -1,18 +1,46 @@
+use std::str::FromStr;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+use tokio;
+use tokio::runtime::Runtime;
+use tokio::time::sleep;
+
 pub struct Room {
     name: String,
+    next_float: Arc<Mutex<f32>>,
 }
 
 impl Room {
     pub fn new(name: &str) -> Self {
-        Room {
+        let next_float = Arc::new(Mutex::new(0.0));
+        let room = Room {
             name: name.to_string(),
-        }
+            next_float: next_float.clone()
+        };
+
+        let next_float2 = next_float.clone();
+        let name2 = String::from_str(name).unwrap();
+
+        let t = thread::spawn(move || {
+
+            let rt  = Runtime::new().unwrap();
+            rt.block_on(async {
+                loop {
+                    sleep(Duration::from_millis(1000)).await;
+                    println!("Hello from {name2}");
+                    let mut x = next_float2.lock().unwrap();
+                    *x = *x+0.01;
+                }
+            });
+        });
+
+        room
     }
 
     pub fn recv_message(&self) -> f32 {
-        let name = &self.name;
-        println!("room name is {name}");
-        32.
+        *self.next_float.lock().unwrap()
     }
 }
 
