6c94357fb5483bca312965b9002c84d279d48be4
Lennart Melzer <lennart@melzer.it>
2024-07-04 22:33:29 +0200

    sc_plugin builds, but fails to load due to openssl not (dynamically) linked

--- src/sc_plugin/src/lib.rs
+++ src/sc_plugin/src/lib.rs
@@ -7,10 +7,17 @@ use tokio;
 use tokio::runtime::Runtime;
 use tokio::time::sleep;
 
+use shared::{
+    api::APIClient,
+    connections::SteckerWebRTCConnection
+};
+
 pub struct Room {
     name: String,
     next_float: Arc<Mutex<f32>>,
 }
+
+const HOST: &str = "http://127.0.0.1:8000";
 
 impl Room {
     pub fn new(name: &str) -> Self {
@@ -27,6 +34,51 @@ impl Room {
 
             let rt  = Runtime::new().unwrap();
             rt.block_on(async {
+                let connection = SteckerWebRTCConnection::build_connection().await.unwrap();
+                let stecker_data_channel = connection.create_data_channel::<String>("foo").await.unwrap();
+                let offer = connection.create_offer().await.unwrap();
+
+                let api_client = APIClient {
+                    host: HOST.to_string(),
+                };
+
+                match api_client.join_room(&name2, &offer).await {
+                    Ok(answer) => {
+                        // Apply the answer as the remote description
+                        connection.set_remote_description(answer).await.unwrap();
+
+                        let mut receiver = stecker_data_channel.inbound.clone().subscribe();
+
+                        tokio::spawn(async move {
+                            loop {
+                                let msg = receiver.recv().await;
+                                match msg {
+                                    Ok(m) => println!("received a message: {m}"),
+                                    Err(_) => println!("Error while receiving message"),
+                                }
+                            }
+                        });
+
+                        println!("Press ctrl-c to stop");
+                        let mut close_receiver = stecker_data_channel.close_trigger.clone().subscribe();
+                        tokio::select! {
+                            _ = close_receiver.recv() => {
+                                println!("received close signal!");
+                            }
+                            _ = tokio::signal::ctrl_c() => {
+                                println!();
+                            }
+                        };
+
+                        connection.close().await.unwrap();
+
+                    }
+                    Err(err) => {
+                        println!("Some error joining room {err}");
+                    },
+                }
+
+
                 loop {
                     sleep(Duration::from_millis(1000)).await;
                     println!("Hello from {name2}");
