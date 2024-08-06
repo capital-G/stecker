ac09de01043c13ab6f2d48872ddcff90171b71c4
Dennis Scheiba <dennis.scheiba@gmx.de>
2024-08-06 12:47:02 +0200

    add cli for server

--- src/server/main.rs
+++ src/server/main.rs
@@ -2,6 +2,8 @@ pub mod models;
 pub mod schema;
 pub mod state;
 
+use std::path::PathBuf;
+
 use crate::schema::{Mutation, Query};
 
 use async_graphql::{http::GraphiQLSource, EmptySubscription, Schema};
@@ -11,9 +13,24 @@ use axum::{
     routing::get,
     Router,
 };
+use clap::Parser;
 use state::AppState;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
+
+const LOCAL_HOST: &str = "127.0.0.1";
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// interface to listen on
+    #[arg(long, default_value_t=LOCAL_HOST.to_string())]
+    host: String,
+
+    /// interface to listen on
+    #[arg(long, default_value_t = 8000)]
+    port: u16,
+}
 
 async fn graphiql() -> impl IntoResponse {
     response::Html(GraphiQLSource::build().endpoint("/").finish())
@@ -21,17 +38,24 @@ async fn graphiql() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
+    let args = Cli::parse();
+
     let schema = Schema::build(Query, Mutation, EmptySubscription)
         .data(AppState::new())
         .finish();
 
     let app = Router::new()
         .route("/", get(graphiql).post_service(GraphQL::new(schema)))
-        .nest_service("/debug", ServeDir::new("./src/server/templates"));
+        .nest_service(
+            "/debug",
+            ServeDir::new(PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("templates")),
+        );
 
-    println!("GraphiQL IDE: http://127.0.0.1:8000");
-
-    axum::serve(TcpListener::bind("0.0.0.0:8000").await.unwrap(), app)
-        .await
-        .unwrap();
+    println!("Start serving on http://{}:{}", args.host, args.port);
+    axum::serve(
+        TcpListener::bind((args.host, args.port)).await.unwrap(),
+        app,
+    )
+    .await
+    .unwrap();
 }
