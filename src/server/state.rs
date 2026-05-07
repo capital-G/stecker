7254c727372b9510ea19fbbe8eb67740d63b295c
Dennis Scheiba <git@dennis-scheiba.com>
2026-05-07 22:57:31 +0200

    use binary location to resolve templates folder location

--- src/server/state.rs
+++ src/server/state.rs
@@ -24,7 +24,11 @@ pub struct AppState {
 impl AppState {
     pub fn new() -> Self {
         let mut env = minijinja::Environment::new();
-        let template_dir = std::env::current_dir().unwrap().join("templates");
+        let template_dir = std::env::current_exe()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("templates");
         env.set_loader(minijinja::path_loader(template_dir));
 
         let (room_event_rx, _) = tokio::sync::broadcast::channel(32);
